7869f259f4479ed40fe2fc6c99229dc0e8954daf
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-05-28 10:30:01 +0300

    Create Decoding.cpp

---
+++ Decoding.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+using namespace std;
+ 
+int main(int argc, char *argv[]) {
+    int n;
+    string str, ans = "";
+    cin >> n >> str;
+    if(n%2 == 1) {
+        ans += str[0];
+        str = str.substr(1, n-1);
+    }
+    int state = 0;
+    while(str.length() > 0) {
+        if(state == 0) {
+            ans = str[0] + ans;
+            state = 1;
+        } else {
+            ans = ans + str[0];
+            state = 0;
+        }
+        str = str.substr(1, str.length() - 1);
+    }
+    cout << ans;
+    return 0;
+}
