448cf20a9653313ca144a72a6ad786fcd0d67c35
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-07-02 01:17:40 +0300

    Update Shifting Stacks.cpp

--- Shifting Stacks.cpp
+++ Shifting Stacks.cpp
@@ -4,7 +4,7 @@
 #define pb              push_back
 #define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
 using namespace std;
-int main() bb
+int main() b
 {
     fastread();
     //freopen("input.txt","r", stdin);
